415cac15501677b70afc113025fd4fcae29e8e56
monabeldi <beldi.mona@esprit.tn>
2022-11-13 12:52:23 +0100

    login

--- app/src/main/java/tn/esprit/onlinelibrary_app/MainActivity.java
+++ app/src/main/java/tn/esprit/onlinelibrary_app/MainActivity.java
@@ -3,6 +3,11 @@ package tn.esprit.onlinelibrary_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView userName = (TextView)findViewById(R.id.username);
+        TextView password = (TextView)findViewById(R.id.password);
+
+        MaterialButton loginBtn = (MaterialButton)findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(userName.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    //correct
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                }else
+                    //incorrect
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
     }
 }
